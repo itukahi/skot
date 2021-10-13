d717b30f52b04d945ff42bbb28156606f8901baa
3401 <3401@skot.com>
2021-10-13 11:31:18 +0900

    ログイン機能を作りました

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,15 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
   resources :diaries
   resources :diary_coments
   resources :teachers
   resources :users
   get 'home/top'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  get '/signup', to:'user#new'
+  get '/login', to:'sessions#new'
+  post '/login', to:'sessions#crea'
+  '/logout', to: 'sessions#destroy'
+  root to: 'home#index'
 end
