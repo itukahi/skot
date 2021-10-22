54b899e410f3892d1d229245330068a565202537
3405 <wakakurumi@icloud.com>
2021-10-22 12:09:36 +0900

    日誌コンテンツ線付けたりしました

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Rails.application.routes.draw do
   resources :diary_coments
   resources :teachers
   resources :users
+  resources :diarytitles
+  
   get '/',to:'home#top'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
