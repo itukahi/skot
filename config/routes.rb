6e9f800bd30b7d4b0b5816d5dd2d043fbdaac9ff
3401 <3401@example>
2021-11-10 10:22:56 +0900

    トップを少し直しました

--- config/routes.rb
+++ config/routes.rb
@@ -14,6 +14,7 @@ Rails.application.routes.draw do
   get '/',to:'home#top'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get 'home/write_or_show'
+  get 'home/top'
   get 'home/search_grade_class'
   get 'home/clicked'
   get '/signup', to:'users#new'
